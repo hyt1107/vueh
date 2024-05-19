c4d3ad0060f2fd94d86ffd78b75552fcba847b45
hyt1107 <pingtin970334@gmail.com>
2024-05-19 23:10:19 +0800

    初版

---
+++ src/layout/AppMenu.vue
@@ -0,0 +1,171 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: 'Home',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+    },
+    {
+        label: 'Data',
+        items: [
+            { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
+            { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
+            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
+            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
+            { label: 'Upload', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
+            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
+        ]
+    },
+    // {
+    //     label: 'Utilities',
+    //     items: [
+    //         { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/utilities/icons' },  //拿icon所在處
+    //         { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://www.primefaces.org/primeflex/', target: '_blank' }
+    //     ]
+    // },
+    {
+        label: 'Auth',
+        icon: 'pi pi-fw pi-briefcase',
+        to: '/pages',
+        items: [
+            // {
+            //     label: 'Landing',
+            //     icon: 'pi pi-fw pi-globe',
+            //     to: '/landing'
+            // },
+            {
+                label: 'Person',
+                icon: 'pi pi-fw pi-user',
+                to: '/pages/person'
+                    // {
+                    //     label: 'Error',
+                    //     icon: 'pi pi-fw pi-times-circle',
+                    //     to: '/auth/error'
+                    // },
+                    // {
+                    //     label: 'Access Denied',
+                    //     icon: 'pi pi-fw pi-lock',
+                    //     to: '/auth/access'
+                    // }
+                // ]
+            },{
+                label: 'Logout',    //login用
+                icon: 'pi pi-fw pi-sign-in',
+                to: '/auth/login'
+            }
+            // {
+            //     label: 'Crud',
+            //     icon: 'pi pi-fw pi-pencil',
+            //     to: '/pages/crud'
+            // },
+            // {
+            //     label: 'Timeline',
+            //     icon: 'pi pi-fw pi-calendar',
+            //     to: '/pages/timeline'
+            // },
+            // {
+            //     label: 'Not Found',
+            //     icon: 'pi pi-fw pi-exclamation-circle',
+            //     to: '/pages/notfound'
+            // },
+            // {
+            //     label: 'Empty',
+            //     icon: 'pi pi-fw pi-circle-off',
+            //     to: '/pages/empty'
+            // }
+        ]
+    },
+    // {
+    //     label: 'Hierarchy',
+    //     items: [
+    //         {
+    //             label: 'Submenu 1',
+    //             icon: 'pi pi-fw pi-bookmark',
+    //             items: [
+    //                 {
+    //                     label: 'Submenu 1.1',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [
+    //                         { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+    //                         { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+    //                         { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+    //                     ]
+    //                 },
+    //                 {
+    //                     label: 'Submenu 1.2',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+    //                 }
+    //             ]
+    //         },
+    //         {
+    //             label: 'Submenu 2',
+    //             icon: 'pi pi-fw pi-bookmark',
+    //             items: [
+    //                 {
+    //                     label: 'Submenu 2.1',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [
+    //                         { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
+    //                         { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
+    //                     ]
+    //                 },
+    //                 {
+    //                     label: 'Submenu 2.2',
+    //                     icon: 'pi pi-fw pi-bookmark',
+    //                     items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // },
+
+
+    // {
+    //     label: 'Get Started',
+    //     items: [
+    //         {
+    //             label: 'Documentation',
+    //             icon: 'pi pi-fw pi-question',
+    //             to: '/documentation'
+    //         },
+    //         {
+    //             label: 'Figma',
+    //             url: 'https://www.dropbox.com/scl/fi/bhfwymnk8wu0g5530ceas/sakai-2023.fig?rlkey=u0c8n6xgn44db9t4zkd1brr3l&dl=0',
+    //             icon: 'pi pi-fw pi-pencil',
+    //             target: '_blank'
+    //         },
+    //         {
+    //             label: 'View Source',
+    //             icon: 'pi pi-fw pi-search',   //搜尋icon要這個
+    //             url: 'https://github.com/primefaces/sakai-vue',
+    //             target: '_blank'
+    //         },
+    //         {
+    //             label: 'Nuxt Version',
+    //             url: 'https://github.com/primefaces/sakai-nuxt',
+    //             icon: 'pi pi-fw pi-star'
+    //         }
+    //     ]
+    // }
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+        <li>
+            <!-- <a href="https://www.primefaces.org/primeblocks-vue/#/" target="_blank">
+                <img src="/layout/images/banner-primeblocks.png" alt="Prime Blocks" class="w-full mt-3" />
+            </a> -->
+        </li>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
